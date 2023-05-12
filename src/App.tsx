f914406490af3818b9554627d444c829ec059c39
avganka <karambasterg@gmail.com>
2023-05-13 01:38:41 +0300

    Собрана главная страница

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import Container from './components/Container/Container';
+import Decoration from './components/Decoration/Decorations';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
 
 function App() {
   return (
-    <Container>
+    <>
+      <Decoration />
       <Header />
-      <main>Content</main>
+      <Container>
+        <main>
+          <Hero />
+        </main>
+      </Container>
       <Footer />
-    </Container>
+    </>
   );
 }
 
