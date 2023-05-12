28cc0d9cc82afb932ba491e245922319a2bfe186
avganka <karambasterg@gmail.com>
2023-05-12 12:48:40 +0300

    Добавлен layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import './App.css';
+import Container from './components/Container/Container';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 
 function App() {
-  return <div>Initial App</div>;
+  return (
+    <Container>
+      <Header />
+      <main>Content</main>
+      <Footer />
+    </Container>
+  );
 }
 
 export default App;
